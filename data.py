0f8ef5cf6c5e83f964824ff1c43390927aaf94cb
Rohit Gaikwad <rgaikwad1998@gmail.com>
2024-04-09 23:29:20 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,84 @@
-
+{
+  "summer-sale-offer": {
+    "title": "Summer Sale Offer",
+    "couponCode": "SUMMER",
+    "offerImgWebm": "../../../assets_tmui/images/summer-sale-webm.jpg",
+    "offerImgWebd": "../../../assets_tmui/images/summer-sale-webd.jpg",
+    "about": {
+      "title": "About the Offer",
+      "couponCode": {
+        "icon": "../../../assets_tmui/icons/coupen.svg",
+        "title": "Coupon Code",
+        "value": "SUMMER",
+        "copyToClipboard": true
+      },
+      "savings": {
+        "icon": "../../../assets_tmui/icons/rupee_circle.svg",
+        "title": "Savings",
+        "savingsList": [
+          {
+            "text": "Get ₹100 off on your First ticket booking"
+          },
+          {
+            "text": "Get ₹25 off on booking again with us"
+          }
+        ]
+      },
+      "validUpto": {
+        "icon": "../../../assets_tmui/icons/calendar.svg",
+        "title": "Valid Upto",
+        "value": "30 June 2024 11:59 PM"
+      }
+    },
+    "benefit": {
+      "title": "How can you get benefit?",
+      "benefitsList": [
+        {
+          "icon": "../../../assets_tmui/icons/person_open.svg",
+          "title": "Create a new account or login to your existing Trainman account"
+        },
+        {
+          "icon": "../../../assets_tmui/icons/wallet.svg",
+          "title": "Use the code SUMMER and get TM Cash worth ₹100 in your TM wallet"
+        },
+        {
+          "icon": "../../../assets_tmui/icons/search.svg",
+          "title": "Search and select a train to your destination"
+        },
+        {
+          "icon": "../../../assets_tmui/icons/cash-back.svg",
+          "title": "Save upto ₹100 on your ticket booking"
+        }
+      ]
+    },
+    "tnc": {
+      "title": "Terms & Conditions",
+      "tncList": [
+        {
+          "condition": "Sign up or Login your Trainman account."
+        },
+        {
+          "condition": "Use the code SUMMER and get TM Cash worth ₹100 to avail the discount."
+        },
+        {
+          "condition": "After applying the code, TM Cash will expire in 60 days."
+        },
+        {
+          "condition": "Avail a discount of ₹100 on your first transaction only."
+        },
+        {
+          "condition": "Avail a discount of ₹25 on booking again with Trainman."
+        },
+        {
+          "condition": "This offer is applicable only for train bookings done on Trainman App & Web."
+        },
+        {
+          "condition": "The minimum ticket value of ₹500 is required to avail the offer."
+        }
+      ]
+    },
+    "cta": {
+      "title": "Apply Now",
+      "link": "https://www.trainman.in/wallet?promo_code=SUMMER"
+    }
+  }
